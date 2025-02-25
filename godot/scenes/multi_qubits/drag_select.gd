a76ac62a877bd1362e0e8cbd70436111401a00e4
Kuift Kotar <kuiftk@gmail.com>
2025-02-25 15:22:33 -0500

    fixed some esoteric bug

--- godot/scenes/multi_qubits/drag_select.gd
+++ godot/scenes/multi_qubits/drag_select.gd
@@ -2,10 +2,30 @@ extends Control
 var dragging = false  # Are we currently dragging?
 var selected = []  # Array of selected units.
 var drag_start = Vector2.ZERO  # Location where drag began.
-var select_rect = RectangleShape2D.new()  # Collision shape for drag box.
 var first_click_position = Vector2()
+enum States {DRAG_SELECT,DRAG}
+var state: States = States.DRAG
 
 func _process(delta):
+	if state == States.DRAG_SELECT:
+		drag_select()
+	elif state == States.DRAG:
+		dragging = false
+		queue_redraw()
+
+
+func _draw():
+	if dragging:
+		draw_rect(Rect2(drag_start, get_global_mouse_position() - drag_start),
+				Color(.5, .5, .5,0.5), true)
+
+func _on_drag_pressed() -> void:
+	state = States.DRAG
+
+func _on_drag_select_pressed() -> void:
+	state = States.DRAG_SELECT
+
+func drag_select():
 	if Input.is_action_just_pressed("click"):
 		first_click_position = get_global_mouse_position()
 
@@ -20,8 +40,3 @@ func _process(delta):
 		queue_redraw()
 	if dragging:
 		queue_redraw()
-
-func _draw():
-	if dragging:
-		draw_rect(Rect2(drag_start, get_global_mouse_position() - drag_start),
-				Color(.5, .5, .5,0.5), true)
