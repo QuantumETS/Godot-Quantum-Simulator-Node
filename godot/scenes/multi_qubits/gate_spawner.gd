5343dd812cf2d0e770b75567d9c0ff07af3ba03d
Kuift Kotar <kuiftk@gmail.com>
2025-03-21 01:02:01 -0400

    added drag and drop gate UI

--- godot/scenes/multi_qubits/gate_spawner.gd
+++ godot/scenes/multi_qubits/gate_spawner.gd
@@ -7,4 +7,17 @@ extends Panel
 @onready var gate = preload("res://scenes/multi_qubits/gate.tscn")
 
 func _on_gui_input(event: InputEvent) -> void:
-	var temp = gate.instantiate()
+	if event is InputEventMouseButton and event.button_mask == 1:
+		var temp = gate.instantiate()
+		add_child(temp)
+		# put the spawner theme onto the temp gate
+		temp.add_theme_stylebox_override("panel", get_theme_stylebox("panel"))
+		
+		temp.global_position = event.global_position-temp.get_rect().size/2
+		temp.process_mode = Node.PROCESS_MODE_DISABLED
+	if event is InputEventMouseMotion and event.button_mask == 1:
+		var moving_gate = get_child(1)
+		moving_gate.global_position = event.global_position-moving_gate.get_rect().size/2
+	if event is InputEventMouseButton and event.button_mask == 0:
+		get_child(1).queue_free()
+	
