a42a59a1328e98f529fba2837b7db3047ca93c75
Kuift Kotar <kuiftk@gmail.com>
2025-03-21 20:43:14 -0400

    added customization option for gates

--- godot/scenes/multi_qubits/gate.gd
+++ godot/scenes/multi_qubits/gate.gd
@@ -1,10 +1,12 @@
 extends Panel
 @export var panel_color: Color
 
-
 func _ready() -> void:
 	get_theme_stylebox("panel").set_bg_color(panel_color)
 
 func change_z_index():
 	z_index = 1
 	$RichTextLabel.z_index = 1
+
+func set_gate_type(type:String):
+	$RichTextLabel.text = type
--- godot/scenes/multi_qubits/gate_spawner.gd
+++ godot/scenes/multi_qubits/gate_spawner.gd
@@ -1,10 +1,29 @@
+@tool
 extends Panel
-@export_enum("X", "Y", "Z", "H", "S", "T","RX(%0.2f)","RY(%0.2f)","RZ(%0.2f)","SWAP","CNOT","custom_controlled") var gate_name: String
-@export var panel_color: Color
+@export_enum("X", "Y", "Z", "H", "S", "T","RX(%0.2f)","RY(%0.2f)","RZ(%0.2f)","SWAP","CNOT","custom_controlled") 
+var gate_name: String:
+	set(value):
+		gate_name = value
+		update_label()
+
+func update_label():
+	if not is_inside_tree():
+		return
+	$RichTextLabel.text = gate_name
+
+
+@export var panel_color: Color:
+	set(value):
+		var sb = get_theme_stylebox("panel").duplicate() as StyleBoxFlat
+		sb.bg_color = value
+		add_theme_stylebox_override("panel", sb)
 #@export var texture : Texture
 #@export var mesh : Mesh
 
 @onready var gate = preload("res://scenes/multi_qubits/gate.tscn")
+
+func _ready() -> void:
+	update_label()
 
 func find_closest_wire(pos: Vector2) -> Node:
 	var closest = null
@@ -20,6 +39,7 @@ func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_mask == 1:
 		var temp = gate.instantiate()
 		add_child(temp)
+		temp.set_gate_type(gate_name)
 		# put the spawner theme onto the temp gate
 		temp.add_theme_stylebox_override("panel", get_theme_stylebox("panel"))
 		
@@ -33,7 +53,7 @@ func _on_gui_input(event: InputEvent) -> void:
 		var wire = find_closest_wire(event.global_position)
 		if wire:
 			remove_child(moving_gate)
-			moving_gate.change_z_index()
+			moving_gate.change_z_index() # move the gate more in the background so subsequent gate aren't hidden by the static gate
 			wire.attach_gate(moving_gate, event.global_position)
 		else:
 			moving_gate.queue_free()
